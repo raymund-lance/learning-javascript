e0d227efced725b1b4f1fd0db1a4a56d90589f06
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-21 22:54:55 +0800

    finished exercise 09 palindrome for javascript

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lettersNumbers = 'abcdefghijklmnopqrstuvwxyz1234567890';
 
+    const cleanString = string
+        .toLowerCase()
+        .split('')
+        .filter((getSameString) => lettersNumbers.includes(getSameString))
+        .join('');
+
+    const reverseString = cleanString.split('').reverse().join('');
+
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
